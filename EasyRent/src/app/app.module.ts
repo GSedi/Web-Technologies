48139c7e7d703432e0e07fdf3999cf6f83e5679d
Ablay <ablay.yedilbayev@gmail.com>
2019-03-19 01:29:08 +0600

    asdf

--- EasyRent/src/app/app.module.ts
+++ EasyRent/src/app/app.module.ts
@@ -3,16 +3,35 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+// import { NavbarComponent } from './navbar/navbar.component';
+
+// import { AlertModule } from 'ngx-bootstrap';
+
+// import { ImageService } from './rent/image.service';
+// import { ImageFilterPipe } from './rent/filter.pipe';
+
+// import { GalleryComponent } from './rent/gallery/gallery.component';
+// import { CarImageComponent } from './rent/car-image/car-image.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    // NavbarComponent,
+    // GalleryComponent,
+    // CarImageComponent
+    // ImageFilterPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    // FormsModule, 
+    // AlertModule.forRoot()
+
   ],
-  providers: [],
+  providers: [
+    //ImageService,
+    //ImageFilterPipe
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
